a2184a47285bb58ce126657b1651a737266d2a2c
JM <jacobmontpetit@gmail.com>
2024-08-10 16:47:47 -0400

    flake: add glow

--- flake.nix
+++ flake.nix
@@ -5,16 +5,16 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              (pkgs.python3.withPackages (python-pkgs: [
-                python-pkgs.black
-                python-pkgs.pynput
-                python-pkgs.scikit-learn
-                python-pkgs.matplotlib
-                python-pkgs.python-lsp-server
-              ]))
-            ];
+          buildInputs = with pkgs; [
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.black
+              python-pkgs.pynput
+              python-pkgs.scikit-learn
+              python-pkgs.matplotlib
+              python-pkgs.python-lsp-server
+            ]))
+            glow
+          ];
         };
       });
 }
